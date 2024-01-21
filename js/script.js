b86a8030e54cec98eca657a709069a6c87cd9174
Calvin Li <65045619+calvin-li-developer@users.noreply.github.com>
2024-01-21 01:59:05 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,6 +3,17 @@ const CIRCLE_CLASS = 'circle';
 const vsComputerText = "COMPUTER";
 const vsPlayerText = "PLAYER 2";
 
+const WINNING_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
 const cellElements = document.querySelectorAll('[data-cell]');
 const board = document.getElementById('board');
 const winningMessageElement = document.getElementById('winning-message');
